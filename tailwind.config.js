17446857505321653872eba2dde18fc07320894e
Linhngo268 <cunlinh268@gmail.com>
2024-08-23 00:56:59 +0930

    1

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./Users/ngolinh/Desktop/LynnNgo/CSHARP/a.html"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
