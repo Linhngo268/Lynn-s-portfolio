59e166db0eb368828d43ff398f4f27475bdbc501
Linhngo268 <cunlinh268@gmail.com>
2024-09-05 15:44:05 +0930

    1

--- app.js
+++ app.js
@@ -17,44 +17,44 @@ app.get('/', (req, res) => {
 });
 
 // Handle form submission
-app.post('/send-email', (req, res) => {
-    const email= req.body.email;
-    const name = req.body.name;
-    const message = req.body.message;
+// app.post('/send-email', (req, res) => {
+//     const email= req.body.email;
+//     const name = req.body.name;
+//     const message = req.body.message;
    
 
 
 
-    // Create a transporter object using the default SMTP transport
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: 'lynnngo2608@gmail.com',
-            pass: 'yqsj mwzz mokf iagt',
-        },
-    });
+//     // Create a transporter object using the default SMTP transport
+//     const transporter = nodemailer.createTransport({
+//         service: 'gmail',
+//         auth: {
+//             user: 'lynnngo2608@gmail.com',
+//             pass: 'yqsj mwzz mokf iagt',
+//         },
+//     });
 
-    // Setup email data
-    const mailOptions = {
-        from:email, // Sender address
-        to: 'lynnngo2608@gmail.com', // List of receivers
-        subject: `Message from ${name} (${email})`, // Subject line
-        text: message, // Plain text body
-        replyTo: email,
-    };
+//     // Setup email data
+//     const mailOptions = {
+//         from:email, // Sender address
+//         to: 'lynnngo2608@gmail.com', // List of receivers
+//         subject: `Message from ${name} (${email})`, // Subject line
+//         text: message, // Plain text body
+//         replyTo: email,
+//     };
 
-    // Send email
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            return res.send('Error: ' + error.message);
-        }
+//     // Send email
+//     transporter.sendMail(mailOptions, (error, info) => {
+//         if (error) {
+//             return res.send('Error: ' + error.message);
+//         }
     
-        res.status(200).json({ message: 'Thank you for your email. I appreciate you taking the time to reach out to me. I will review your message and respond to you as soon as possible.Thank you for your understanding and patience.</p>' });
+//         res.status(200).json({ message: 'Thank you for your email. I appreciate you taking the time to reach out to me. I will review your message and respond to you as soon as possible.Thank you for your understanding and patience.</p>' });
          
         
          
-    });
-});
+//     });
+// });
 
 
 module.exports = app;
